b73688e23cc82a808e1f62bb8a6cab3151283290
islammohsen <islammohsenmohamed.1998@gmail.com>
2019-12-13 16:32:03 +0200

    Added edit pressure measure

---
+++ Blood-Pressure-Tracker/Controllers/PressureMeasureController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Blood_Pressure_Tracker.Models;
+using Blood_Pressure_Tracker.View_Models;
+
+namespace Blood_Pressure_Tracker.Controllers
+{
+    public class PressureMeasureController : Controller
+    {
+        private ApplicationDbContext dbContext;
+
+        public PressureMeasureController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            dbContext.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult EditPressureMeasure(int id)
+        {
+            PressureMeasure pressureMeasure = dbContext.PressureMeasures.SingleOrDefault(m => m.Id == id);
+            EditPressureMeasureViewModel editPressureMeasureViewModel = new EditPressureMeasureViewModel
+            {
+                Id = id,
+                Date = pressureMeasure.Date,
+                Diastole = pressureMeasure.Diastole,
+                Systole = pressureMeasure.Systole
+            };
+            return View(editPressureMeasureViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditPressureMeasure(EditPressureMeasureViewModel model)
+        {
+            PressureMeasure pressureMeasure = dbContext.PressureMeasures.SingleOrDefault(m => m.Id == model.Id);
+            pressureMeasure.Diastole = model.Diastole;
+            pressureMeasure.Systole = model.Systole;
+            dbContext.SaveChanges();
+            return RedirectToAction("Index", "Dashboard", new { email = User.Identity.Name});
+        }
+    }
+}
---
+++ Blood-Pressure-Tracker/View Models/EditPressureMeasureViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blood_Pressure_Tracker.View_Models
+{
+    public class EditPressureMeasureViewModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Diastole { get; set; }
+        public int Systole { get; set; }
+    }
+}
